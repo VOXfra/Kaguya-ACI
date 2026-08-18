9fb8c64c6afccb8f7e8d0de5fcd006821266097d
VOXfra <shift.partenariat@gmail.com>
2026-08-18 14:56:06 +0200

    temp: CardSim v0.5 version

--- cardsim-v03/app/build.gradle.kts
+++ cardsim-v03/app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
         applicationId = "com.vox.cardsim"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.4.0"
+        versionCode = 5
+        versionName = "0.5.0"
     }
 }
