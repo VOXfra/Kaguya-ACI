21b14f7dad401465f2f61cfa811b1c57c9346174
VOXfra <shift.partenariat@gmail.com>
2026-08-18 13:26:01 +0200

    temp: CardSim 151 v0.2 source tree

---
+++ cardsim-v02/app/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "com.vox.cardsim"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.vox.cardsim"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 2
+        versionName = "0.2.0"
+    }
+}
