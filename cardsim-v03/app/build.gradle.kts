ab0808afb594ed7c2c56713c9b16d82559985727
VOXfra <shift.partenariat@gmail.com>
2026-08-19 12:08:06 +0200

    chore(cardsim): bump version to 0.8.8

--- cardsim-v03/app/build.gradle.kts
+++ cardsim-v03/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.vox.cardsim"
         minSdk = 26
         targetSdk = 35
-        versionCode = 21
-        versionName = "0.8.7"
+        versionCode = 22
+        versionName = "0.8.8"
     }
 
     compileOptions {
